79c5e81651fefe20242eab32385b761fb68404f8
Mercy Oriaku <berenonso45@gmail.com>
2017-03-29 14:09:52 +0100

    modified header

--- www/includes/header.php
+++ www/includes/header.php
@@ -2,7 +2,8 @@
 <html>
 <head>
 	<title>Register</title>
-	<link rel="stylesheet" type="text/css" href="styles/styles.css">
+	<link rel="stylesheet" type="text/css" href="
+	../styles/styles.css">
 </head>
 <body>
 	<section>
